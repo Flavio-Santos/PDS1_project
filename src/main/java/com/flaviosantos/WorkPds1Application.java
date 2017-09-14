3caeed15bb1be37fc650b95ecfc70c8bda5a20ed
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-09-14 20:09:47 -0300

    Projeto Iniciado

---
+++ src/main/java/com/flaviosantos/WorkPds1Application.java
@@ -0,0 +1,12 @@
+package com.flaviosantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPds1Application.class, args);
+	}
+}
