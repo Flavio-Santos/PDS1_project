f7ee5c71d422341d02b53297cb1a35c2684fd0ed
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-09-21 21:25:41 -0300

    operacao de instanciacao criada

--- src/main/java/com/flaviosantos/WorkPds1Application.java
+++ src/main/java/com/flaviosantos/WorkPds1Application.java
@@ -1,12 +1,32 @@
 package com.flaviosantos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkPds1Application {
+import com.flaviosantos.domain.Categoria;
+import com.flaviosantos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WorkPds1Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepositorie;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkPds1Application.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepositorie.save(Arrays.asList(cat1,cat2));
+		
 	}
 }
