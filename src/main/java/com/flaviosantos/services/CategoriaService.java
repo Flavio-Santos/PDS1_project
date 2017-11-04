98ea29e4ee5a41712394f4e3d0f46fdeaa5376ec
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-04 20:36:06 -0200

    deletando uma categoria com DELETE

--- src/main/java/com/flaviosantos/services/CategoriaService.java
+++ src/main/java/com/flaviosantos/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.flaviosantos.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.flaviosantos.domain.Categoria;
 import com.flaviosantos.repositories.CategoriaRepository;
+import com.flaviosantos.services.exceptions.DataIntegrityException;
 import com.flaviosantos.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 		find(obj.getId());
 		
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id){
+		find(id);
+		try{
+			repo.delete(id);			
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel Excluir uma Categoria que possui produtos");
+		}
 	}
 }
