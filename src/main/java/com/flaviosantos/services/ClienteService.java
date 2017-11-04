6f3e83da782abaef96f64d01b25860b693947594
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-04 14:49:18 -0200

    inserindo uma nova Categoria com post

--- src/main/java/com/flaviosantos/services/ClienteService.java
+++ src/main/java/com/flaviosantos/services/ClienteService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.flaviosantos.domain.Cliente;
-import com.flaviosantos.domain.Cliente;
 import com.flaviosantos.repositories.ClienteRepository;
 import com.flaviosantos.services.exceptions.ObjectNotFoundException;
 
