0e56f63ef22919785fc2e5c7f175ce2666ac9d23
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-28 18:25:37 -0200

    restricao de conteudo, cliente recupera so ele mesmo

--- src/main/java/com/flaviosantos/config/SecurityConfig.java
+++ src/main/java/com/flaviosantos/config/SecurityConfig.java
@@ -56,9 +56,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-			.antMatchers(HttpMethod.
-			GET, PUBLIC_MATCHERS_GET).permitAll()
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
--- src/main/java/com/flaviosantos/services/ClienteService.java
+++ src/main/java/com/flaviosantos/services/ClienteService.java
@@ -13,18 +13,21 @@ import org.springframework.stereotype.Service;
 import com.flaviosantos.domain.Cidade;
 import com.flaviosantos.domain.Cliente;
 import com.flaviosantos.domain.Endereco;
+import com.flaviosantos.domain.enums.Perfil;
 import com.flaviosantos.domain.enums.TipoCliente;
 import com.flaviosantos.dto.ClienteDTO;
 import com.flaviosantos.dto.ClienteNewDTO;
 import com.flaviosantos.repositories.CidadeRepository;
 import com.flaviosantos.repositories.ClienteRepository;
 import com.flaviosantos.repositories.EnderecoRepository;
+import com.flaviosantos.security.UserSS;
+import com.flaviosantos.services.exceptions.AuthorizationException;
 import com.flaviosantos.services.exceptions.DataIntegrityException;
 import com.flaviosantos.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
 	@Autowired
@@ -33,68 +36,74 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private BCryptPasswordEncoder pe;
-	
+
 	public Cliente find(Integer id) {
-		Cliente obj = repo.findOne(id);
+		UserSS user = UserService.authenticated();
 		
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		Cliente obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado id: "+id+ ", tipo: "+Cliente.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado id: " + id + ", tipo: " + Cliente.class.getName());
 		}
 		return obj;
 	}
-	
-	public Cliente insert(Cliente obj){
+
+	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
 		enderecoRepository.save(obj.getEnderecos());
 		return obj;
 	}
-	
-	public Cliente update(Cliente obj){
+
+	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
-	
-	public void delete(Integer id){
+
+	public void delete(Integer id) {
 		find(id);
-		try{
-			repo.delete(id);			
-		}
-		catch (DataIntegrityViolationException e) {
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel Excluir porque há pedidos relacionados");
 		}
 	}
-	
-	public List<Cliente> findAll(){
+
+	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
-	public Cliente fromDTO(ClienteDTO objDto){
-		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);		
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
-	public Cliente fromDTO(ClienteNewDTO objDto){
-		 Cliente cli = new Cliente(null , objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
-		 Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
-		 Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
-		 cli.getEnderecos().add(end);
-		 cli.getTelefones().add(objDto.getTelefone1());
-		 if(objDto.getTelefone2()!=null){
-			 cli.getTelefones().add(objDto.getTelefone2());
-		 }
-		 if(objDto.getTelefone3()!=null){
-			 cli.getTelefones().add(objDto.getTelefone3());
-		 }
-		 return cli;
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
-	
-	private void updateData(Cliente newObj, Cliente obj){
+
+	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
