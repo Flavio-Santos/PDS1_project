edad9fcd1ef619f3c1a4a3d5776987317c49b6e2
Flavio-Santos <flavio.santos.pro@gmail.com>
2017-11-28 19:15:33 -0200

    esqueci a senha

--- src/main/java/com/flaviosantos/services/EmailService.java
+++ src/main/java/com/flaviosantos/services/EmailService.java
@@ -2,6 +2,7 @@ package com.flaviosantos.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.flaviosantos.domain.Cliente;
 import com.flaviosantos.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
